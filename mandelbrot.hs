11692f110e40c2dbc3dfc0a32a6796283311aede
Derek VerLee <derekverlee@gmail.com>
2012-06-11 02:16:00 -0400

    Cleanup, doc

--- mandelbrot.hs
+++ mandelbrot.hs
@@ -1,16 +1,16 @@
+-- Generates the Mandlebrot set
+
 import Data.Complex
 import List
 
---mandelbrotFindChar :: t -> Char = _
---mandelbrotFindItors :: RealFloat a => Complex a -> Int -> Maybe Int = _
-
---mandl :: Num a => a -> [a] = _
 mandl a = iterate (\z -> z^2 + a) a 
 
 mandelbrotFindItors z max = findIndex (\x->(magnitude x) > 2) (take max (mandl z))
 mandelbrotFindChar Nothing = '*'
 mandelbrotFindChar (Just x) = ' '
 
---mapM_ putStrLn 
+--fullset creates a ascii representation of the set 
+--to print it out to the screen, 
+--mapM_ putStrLn fullset 
 
 fullset = [[if (mandelbrotFindItors (x :+ y) 100) == Nothing then '*' else ' ' | x <- [-2, -1.9685 .. 0.5]] |  y <- [1, 0.95 .. -1]]
